30d7cc66cb30d23e1fb6c61458654642a9b6b830
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-12-31 20:22:08 -0300

    2

---
+++ 2/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+    export default{
+        setup(){
+            const message = "Hola VUE!"
+            return {
+                message
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
