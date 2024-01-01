e3b8e44390a764a7efbed25d76c5ee5a1e605e08
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-01 16:42:32 -0300

    5 - Ciclos de vida

---
+++ 5/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+    import { onBeforeMount, onBeforeUpdate, onBeforeUnmount, onMounted, onUpdated, onUnmounted } from "vue"
+    export default{
+        setup(){
+            onBeforeMount(() => console.log("onBeforeMount"))
+            onMounted(() => console.log("onMounted"))
+            onBeforeUpdate(() => console.log("onBeforeUpdate"))
+            onUpdated(() => console.log("onUpdated"))
+            onBeforeUnmount(() => console.log("onBeforeUnmounted"))
+            onUnmounted(() => console.log("onUnmounted"))
+        }
+    }
+</script>
+
+<template>
+    
+</template>
