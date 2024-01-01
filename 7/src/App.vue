3aa07139f5acf32028fa6073521e4976150a58b6
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-01 17:09:48 -0300

    7 - Watchers

---
+++ 7/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+    import { ref, watch } from "vue"
+    export default{
+        setup(){
+            const message = ref("Hola vue!")
+            watch(message, (newValue, oldValue) =>{
+                console.log('oldvalue =>',oldValue,' newvalue =>',newValue)
+            })
+            return {
+                message
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+    <input type="text" v-model="message">
+</template>
