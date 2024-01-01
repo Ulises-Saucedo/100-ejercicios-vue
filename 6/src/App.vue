3ae11ec8916dcd8ddd175d593b6a703cf98aa73b
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-01 16:54:58 -0300

    6 - Data binding

---
+++ 6/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+    import { ref } from "vue"
+    export default{
+        setup(){
+            const title = ref('')
+            const post = ref('')
+            return {
+                title,
+                post,
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>My vue blog</h1>
+    <h2>Nueva entrada</h2>
+    <h3>Titulo</h3><input type="text" v-model="title">
+    <textarea cols="50" v-model="post"></textarea>
+    <h2>{{ title }}</h2>
+    <p>{{ post }}</p>
+</template>
