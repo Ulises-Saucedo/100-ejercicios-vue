a412c3e070e2ec77772cf24771eede1d908732b5
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-01 18:06:44 -0300

    9 - Métodos

---
+++ 9/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+    export default{
+        setup(){
+            const title = "Hola vue"
+            const showMessage = () =>{
+                console.log(`Showing message: ${title} Aprendamamos!`)
+                return `Showing message: ${title} Aprendamamos!`
+            }
+            return{
+                showMessage
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ showMessage() }}</h1>
+    <button @click="showMessage">Mostrar titulo</button>
+</template>
