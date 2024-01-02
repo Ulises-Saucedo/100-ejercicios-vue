a336de8228d3e1877ea8871f9add7a33ba1bc4c7
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-02 14:11:32 -0300

    19 - Mouse move

---
+++ 19/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+  import { ref } from "vue"
+  export default {
+    setup(){
+      const mouseX = ref(0)
+      const mouseY = ref(0)
+      function move(e){
+        mouseX.value = e.clientX
+        mouseY.value = e.clientY
+      }
+      return {
+        mouseX,
+        mouseY,
+        move
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>Mouse move</h1>
+  <div class="mousearea" @mousemove="move"></div>
+  <div>{{ mouseX }} - {{ mouseY }}</div>
+</template>
+
+<style scoped>
+  .mousearea{
+    width: 300px;
+    border: 15px solid green;
+    padding: 50px;
+    margin: 20px;
+  }
+</style>
