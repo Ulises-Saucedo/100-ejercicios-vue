872e17a1b872d5b287f9b2bcd51df39988129178
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-02 11:57:28 -0300

    13 - Vincular propiedades (v-bind)

---
+++ 13/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+    import { ref } from 'vue'
+    export default{
+        setup(){
+            const image = ref('https://via.placeholder.com/320x240.png')
+            const index = ref(0)
+            const handleClick = () =>{
+                image.value = `https://loremflickr.com/320/240?random=${index.value++}`
+            }
+            return {
+                handleClick,
+                image
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>v-bind image</h1>
+    <button @click="handleClick">Change image!</button>
+    <!-- <img :src="image"> -->
+    <img v-bind:src="image">
+</template>
