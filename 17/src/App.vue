689710778615cd626b6745d4a64ba522b36b63dd
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-02 13:50:29 -0300

    17 - Filtros

---
+++ 17/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+  import { ref } from 'vue'
+  export default{
+    setup(){
+      const newProduct = ref('')
+      const shoppingList = ref(["manzana", "leche", "hamburguesas", "patatas fritas"])
+      const onEnterPressed = () =>{
+        shoppingList.value.push(newProduct.value)
+        newProduct.value = ''
+      }
+      const convertirMayusculas = (text) => {
+        return text.toUpperCase()
+      }
+      return {
+        newProduct,
+        shoppingList,
+        onEnterPressed,
+        convertirMayusculas
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>v-for</h1>
+  <h2>Lista de compras</h2>
+  <input type="text" @keyup.enter="onEnterPressed" v-model="newProduct">
+  <ul>
+    <li v-for="product in shoppingList">
+      {{ convertirMayusculas(product) }}
+    </li>
+  </ul>
+</template>
