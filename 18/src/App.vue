83f6bbcfeab923abf4fd90dec2825cd28e8d3162
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-02 13:57:46 -0300

    18 - Renderizar solo una vez

---
+++ 18/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+  import { ref } from 'vue'
+  export default{
+    setup(){
+        const message = ref('Solo una vez')
+        return {
+          message
+        }
+      }
+    }
+</script>
+
+<template>
+  <h1 v-once>{{ message }}</h1>
+  <input type="text" v-model="message">
+</template>
