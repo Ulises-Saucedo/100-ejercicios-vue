b0622ed24fd788909901cb0364f8bc635678eecc
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-02 13:43:03 -0300

    16 - Enumeración de propiedades

---
+++ 16/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  export default{
+    setup(){
+      const post = {
+        title: "Nuevo artículo",
+        date: "24/03/2019",
+        author: "Ramón"
+      }
+      return {
+        post
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>v-for object</h1>
+  <ul>
+    <li v-for="(value, key, index) in post">
+      {{ index + 1 }} => {{ key }}: {{ value }}
+    </li>
+  </ul>
+</template>
