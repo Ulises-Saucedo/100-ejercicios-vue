01cda815b2acd7f153a2d34582ef519465cfe6ef
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-02 11:35:58 -0300

    12 - Renderizar HTML

---
+++ 12/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+    export default{
+        setup(){
+            const html = `
+                <div>
+                    <h1>Ejemplo de renderizado HTML</h1>
+                    este enlace nos lleva a <a href="https://google.com">Google</a> 
+                </div>
+            `
+            return {
+                html
+            }
+        }
+    }
+</script>
+
+<template>
+    <div v-html="html"></div>
+</template>
