c065514e72d9b1434e1e781accdef138f6cc748f
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-02 11:26:17 -0300

    11 - Eventos

---
+++ 11/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+    import { ref } from 'vue';
+    export default{
+        setup(){
+            const enterMessage = ref('')
+            const inputText = ref('')
+            const onEnterPressed = () =>{
+                enterMessage.value = inputText.value
+            }
+            return {
+                message: "Escuchando evento teclado [Enter]",
+                enterMessage,
+                inputText,
+                onEnterPressed
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+    <input type="text" @keyup.enter="onEnterPressed" v-model="inputText">
+    <p>{{ enterMessage }}</p>
+</template>
