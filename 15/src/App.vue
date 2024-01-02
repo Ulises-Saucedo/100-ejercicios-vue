ce1692f04d345cf3679b0c1b4d5265b976eeb5e7
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-02 13:26:42 -0300

    15 - Listas

---
+++ 15/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+  import { ref } from 'vue'
+  export default{
+    setup(){
+      const newProduct = ref('')
+      const shoppingList = ref(["manzana", "leche", "hamburguesas", "patatas fritas"])
+      const onEnterPressed = () =>{
+        shoppingList.value.push(newProduct.value)
+        newProduct.value = ''
+      }
+      return {
+        newProduct,
+        shoppingList,
+        onEnterPressed
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>v-for</h1>
+  <h2>Lista de compras</h2>
+  <input type="text" @keyup.enter="onEnterPressed" v-model="newProduct">
+  <ul>
+    <li v-for="product in shoppingList">
+      {{ product }}
+    </li>
+  </ul>
+</template>
