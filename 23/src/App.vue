f91cf6e90b363272524a8696f066cee39c3e4ee7
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-03 20:16:41 -0300

    23 - Directivas personalizadas IV

---
+++ 23/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+    setup(){
+    },
+    directives: {
+      format: {
+        mounted: (el, binding) =>{
+          const modifiers = binding.modifiers
+          if(modifiers.underline){
+              el.style.textDecoration = 'underline'
+          }
+          if(modifiers.bold){
+              el.style.fontWeight = 'bold'
+          }
+          if(modifiers.highlight){
+              el.style.background = '#FF00FF'
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1 v-format.bold.underline.highlight>TITULO</h1>
+  <h2 v-format.bold.underline.highlight>SUBTITULO</h2>
+</template>
