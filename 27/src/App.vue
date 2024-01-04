21055e8c4fd40a1af54442d219d38aa858ce31c5
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-04 03:01:37 -0300

    27 - Cambiar valor de props

---
+++ 27/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import { ref } from "vue"
+  import Props from "./components/Props.vue"
+  export default {
+    components: {
+      Props
+    },
+    setup(){
+      const valueData = ref('Valor inicial')
+      return { 
+        valueData
+      }
+    } 
+  }
+</script>
+
+<template>
+  <input type="text" v-model="valueData" />
+  <Props :propValue="valueData" />
+</template>
---
+++ 27/src/components/Props.vue
@@ -0,0 +1,25 @@
+<script>
+  import { ref, watch } from "vue"
+  export default {
+    props: {
+      propValue: {
+        type: String,
+        required: true,
+      }
+    },
+    setup(props){
+      const myValue = ref(props.propValue)
+      watch(() => props.propValue, (newPropValue) =>{
+        myValue.value = newPropValue
+      })
+      return { 
+        props,
+        myValue
+      }
+    }
+  }
+</script>
+
+<template>
+  <p>Valor: {{ myValue }}</p>
+</template>
