d6926aef9bc910bbf1728cbbf04834701ecbe83f
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-04 20:55:25 -0300

    37 - Inline template (En vue 3 no existe este concepto)

---
+++ 37/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+    setup() {
+      const title = 'Ejemplo con inline-template';
+      const message = 'Este es un componente Vue con inline-template.';
+      const templateContent = `
+        <div>
+          <h1>${ title }</h1>
+          <p>${ message }</p>
+        </div>
+      `;
+      return { 
+        templateContent, 
+        title, 
+        message 
+      };
+    },
+  };
+</script>
+
+<template>
+  <div v-html="templateContent"></div>
+</template>
