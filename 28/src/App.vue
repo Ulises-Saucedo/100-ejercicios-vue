3ac269dd5a9744de8aa94a49473962e8ee03c762
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-04 14:22:00 -0300

    28 - Reactividad Data

---
+++ 28/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import Props from "./components/Props.vue"
+  export default {
+    components: {
+      Props
+    },
+    setup(){
+
+      return {
+
+      }
+    }
+  }
+</script>
+
+<template>
+  <Props :propPerson="{FirstName: 'Ramón', LastName: 'Serrano'}"/>
+</template>
---
+++ 28/src/components/Props.vue
@@ -0,0 +1,47 @@
+<script>
+    import { ref } from "vue"
+    export default {
+        props: {
+            propPerson: {
+                type: Object
+            }
+        },
+        setup(props){
+            const person = ref(props.propPerson)
+            const name = ref(null)
+            const age = ref(null)
+            const onChangeName = () => {
+                person.value.FirstName = name.value
+            }
+            const deleteFirstName = () => {
+                delete person.value.FirstName
+            }
+            const addAge = () => {
+                person.value.Age = age.value
+            }
+            return {
+                person,
+                age,
+                name,
+                onChangeName,
+                deleteFirstName,
+                addAge
+            }
+        }
+    }
+</script>
+
+<template>
+    <p v-if="person">
+        <b>(Reactividad Objeto)Persona:</b>
+        <span>{{ person.FirstName }} {{ person.LastName }} {{ person.Age }}</span>
+    </p>
+    <input type="text" placeholder="Nombre" v-model="name" @change="onChangeName">
+    <p>
+        <b>(Reactividad)Edad:</b>
+        {{ age }}
+    </p>
+    <input type="number" placeholder="Edad" v-model="age">
+    <input type="button" @click="deleteFirstName" value="Borra nombre">
+    <input type="button" @click="addAge" value="Añade edad">
+</template>
