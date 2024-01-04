2d293e36b8688196634189c4e8423276bb194f50
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-04 18:49:16 -0300

    34 - 'Contexto This (composition api)'

---
+++ 34/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import { ref, onMounted } from "vue"
+  export default {
+    setup(){
+      const message = ref('Mensaje data')
+      function fRegular(){ console.log(message.value) }
+      const fRegularArrow = () =>{ console.log(message.value) }
+      onMounted(() =>{
+        fRegular()
+        fRegularArrow()
+      })
+      return {
+        message,
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>Contexto this</h1>
+</template>
