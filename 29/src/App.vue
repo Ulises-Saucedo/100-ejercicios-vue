ba9851ac8641d025622f82008c5fb9eba3c254ff
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-04 15:15:05 -0300

    29 - Reactividad NextTick

---
+++ 29/src/App.vue
@@ -0,0 +1,30 @@
+<script>
+  import { ref, watch, nextTick } from "vue"
+  export default {
+    setup(){
+      const focus = ref(false)
+      const nombreRef = ref(null)
+      const doFocus = () =>{
+        focus.value = false
+        nextTick(() => {
+          focus.value = true
+        })
+      }
+      watch(() =>{
+        console.log("Focus: ", focus.value)
+        if(focus.value) nombreRef.value.focus()
+      })
+      return {
+        focus,
+        doFocus,
+        nombreRef
+      }
+    }
+  }
+</script>
+
+<template>
+  <p>Focus: {{ focus }}</p>
+  <input type="text" ref="nombreRef" value="Ramón">
+  <input type="button" @click="doFocus" value="Hacer foco">
+</template>
