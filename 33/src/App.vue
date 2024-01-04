295123dbcaa8b7c8d0105c50df8388b2b216651a
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-04 18:23:45 -0300

    33 - Peticiones fetch

---
+++ 33/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import { ref, onMounted } from "vue"
+  export default {
+    setup(){
+      const series = ref([])
+      onMounted(async() =>{
+        const response = await fetch('https://vue-100-ejercicios-cli-series.firebaseio.com/json')
+        const json = await response.json()
+        series.value = json
+      })
+      return {
+        series
+      }
+    }
+  }
+</script>
+
+<template>
+  <h4>Fetch</h4>
+  <ul>
+    <li v-for="serie in series">{{ serie.Titulo }}</li>
+  </ul>
+</template>
