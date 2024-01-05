e1f9708c12b427d8d18c640ff84ff7b3a78f8194
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-04 21:29:59 -0300

    38 - Slots

---
+++ 38/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  import Post from "./layouts/Post.vue"
+  export default {
+    components: {
+      Post
+    },
+    setup(){
+      return {
+        header: "Article",
+        image: "https://lorempixel.com/400/200"
+      }
+    }
+  }
+</script>
+
+<template>
+  <Post>
+    <template v-slot:content>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis ab asperiores mollitia quidem eius sint impedit magnam laudantium molestiae commodi officiis natus assumenda, minus, voluptate nostrum nobis porro alias sapiente?</p>
+    </template>
+  </Post>
+</template>
---
+++ 38/src/layouts/Post.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    setup() {
+        return {
+            header: "Article",
+            image: "https://media.istockphoto.com/id/1434218035/es/foto/antena-suburbana-de-austin.jpg?s=2048x2048&w=is&k=20&c=XU8W1mpQ0oXbvzewTZz4ro4ndO3q3lvwB5v7sBIFBps="
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ header }}</h1>
+    <img :src="image">
+    <slot name="content"></slot>
+</template>
