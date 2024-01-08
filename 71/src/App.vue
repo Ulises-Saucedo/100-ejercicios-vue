86749d105845933f6ad668c0cd9594b327ea9061
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-08 10:20:09 -0300

    71 - Comunicación con servidor

---
+++ 71/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import axios from 'axios'
+  export default {
+    setup(){
+      const data = async () => {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+        console.log(response)
+      }
+      return {
+        data
+      }
+    }
+  }
+</script>
+
+<template>
+  <button @click="data">Traer data (consola)</button>
+</template>
