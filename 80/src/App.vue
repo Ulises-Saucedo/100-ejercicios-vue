3d0a1bddc0bf8b4d7910cb7863a8630b379cce3d
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-08 12:46:02 -0300

    80 - Comunicación (Hijo - Padre por callback)

---
+++ 80/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Componente Padre</h2>
+    <Hijo @mensaje-al-padre="manejarMensajeDelHijo" />
+    <p>Mensaje del Hijo: {{ mensajeDelHijo }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import Hijo from './components/Hijo.vue';
+
+export default {
+  components: {
+    Hijo,
+  },
+  setup() {
+    const mensajeDelHijo = ref('')
+    const manejarMensajeDelHijo = (mensaje) => {
+      mensajeDelHijo.value = mensaje
+    }
+    return {
+      mensajeDelHijo,
+      manejarMensajeDelHijo,
+    }
+  },
+}
+</script>
---
+++ 80/src/components/Hijo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button @click="enviarMensajeAlPadre">Enviar Mensaje al Padre</button>
+    </div>
+</template>
+
+<script>
+    import { ref } from 'vue'
+    export default {
+        setup(props, { emit }) {
+            const enviarMensajeAlPadre = () => {
+                emit('mensaje-al-padre', '¡Hola desde el hijo!')
+            };
+            return {
+                enviarMensajeAlPadre,
+            }
+        },
+    }
+</script>  
