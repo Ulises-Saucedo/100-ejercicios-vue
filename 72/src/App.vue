2d2bbeba41b27f7c9fddbad74c98ee5e422db4ea
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-08 10:29:09 -0300

    72 - Axios (Cancelar solicitud)

---
+++ 72/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+  import axios from 'axios'
+  export default {
+    setup(){
+      const cancelTokenSource = axios.CancelToken.source()
+      const data = async () => {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts", {
+          cancelToken: cancelTokenSource.token
+        })
+        console.log(response)
+      }
+      const cancelRequest = () => {
+        cancelTokenSource.cancel('Solicitud cancelada manualmente')
+      }
+      return {
+        data,
+        cancelRequest
+      }
+    }
+  }
+</script>
+
+<template>
+  <button @click="data">Traer data (consola)</button>
+  <button @click="cancelRequest">Cancelar solicitud</button>
+</template>
